e7160b060ecaad358777a28ddd3b0dc7e1c17b76
italobarrosme <italobarros.me@gmail.com>
2022-07-02 17:02:14 -0300

    fix: fix bug '

--- pages/404.tsx
+++ pages/404.tsx
@@ -25,7 +25,7 @@ const NotFound: NextPage = () => {
             <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
               <span className="sr-only">Error</span>404
             </h2>
-            <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+            <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldnt find this page.</p>
             <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
             <Link href="/">
               <a className={styles.a}><Icon icon={'bi:arrow-left-circle'} />Back to Home </a>

a5cb9e630c5bec57419319605553c797d0729962
italobarrosme <italobarros.me@gmail.com>
2022-08-23 10:18:01 -0300

    feat: :art: add clsx add useHooksTs add mobile menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image';
+// import Image from 'next/image';
 import { Icon } from '@iconify/react';
 import { useRouter } from 'next/router';
 import styles from './Navbar.module.scss'
+import { useWindowSize } from 'usehooks-ts'
+import clsx from 'clsx'
 
 const pages = [
   {
@@ -24,19 +26,21 @@ const pages = [
 ]
 
 
-const Navbar = () => {
-
+export const Navbar = () => {
   const router = useRouter();
+  const { width, height } = useWindowSize()
 
   return (
     <>
       <nav className={styles.navbar_list}>
 
-        <div className={styles.logo}>
-          <Icon icon="simple-icons:pcgamingwiki" />
-          {/* <Image src="/logo.png" width={'64'} height={'64'} alt="logo" /> */}
-        </div>
-        <ul className={styles.items_list}>
+        {width > 1080 &&
+          <div className={styles.logo}>
+            <Icon icon="simple-icons:pcgamingwiki" />
+            {/* <Image src="/logo.png" width={'64'} height={'64'} alt="logo" /> */}
+          </div>
+        }
+        <ul className={clsx(width < 1080 ? styles.items_list_mobile : styles.items_list_desktop)}>
           {pages.map(page => (
             <li className={router.pathname === page.href ? styles.__active : ''} key={page.name}>
               <Link href={page.href}>
@@ -52,5 +56,3 @@ const Navbar = () => {
     </>
   )
 }
-
-export default Navbar;
